56035a7ead5ee150408a0d67e3f71fab13a8d3d6
ShubhamYv <shubhamyadav32100@gmail.com>
2024-09-26 22:29:25 +0530

    Initial commit

---
+++ src/main/java/uber_auth_service/UberAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package uber_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberAuthServiceApplication.class, args);
+	}
+
+}
